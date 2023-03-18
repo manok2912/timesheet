7196ef72e76c72c1f001dce2fb257f274ce73555
manoj <manojkumarch2020@gmail>
2023-03-18 17:50:22 +0530

    test

--- src/index.js
+++ src/index.js
@@ -5,12 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( <
-    React.StrictMode >
-    <
-    App / >
-    <
-    /React.StrictMode>
+root.render( 
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start testt measuring performance in your app, pass a function
